feba1dcca84cd381478803fb57aef76df3ee3647
Jai Hyun Park <jhyunp@Jaiui-MacBookPro.local>
2023-04-12 10:10:26 +0900

    sync

--- src/main/main-prime.cpp
+++ src/main/main-prime.cpp
@@ -107,24 +107,68 @@ int EXP = EXP_LOGN16;
         
         int num = 0;
         //for (uint32 prime = ((uint32) 1 << bitlen) + 0x1 - ((uint32) 1 << (logN + 1)); prime >> (bitlen - 1); prime -= (uint32) 1 << (logN + 1)) {
+        
         std::vector<uint32> primes;
-
+        std::vector<long double> log_primes;
+        std::vector<int> prime_pair;
+        
+        long double log_avg = 0;
         for (uint32 prime = ((uint32) 1 << (bitlen - 1)) + 0x1; !(prime >> bitlen); prime += (uint32) 1 << (logN + 1)) {
             if (is_prime(prime)) {
                 if (std::__popcount(prime) > 7)
                     continue;
                 num ++;
-                std::cout << std::hex << prime << " (" << std::dec << std::__popcount(prime) << ")" << std::endl;
+//                std::cout << std::hex << prime << " (" << std::dec << std::__popcount(prime) << ")" << std::endl;
                 primes.push_back(prime);
+                log_primes.push_back(logl(prime));
+                prime_pair.push_back(-1);
+                log_avg += logl(prime);
+            }
+        }
+        log_avg /= num;
+
+        std::cout << num << std::endl;
+        std::cout << log_avg << std::endl;
+
+        for (int i = 0; i < num; i ++) {
+            long double min_log = 999999;
+            for (int j = 0; j < num; j ++) {
+                if (i == j)
+                    continue;
+                long double diff = abs(log_avg * 2 - log_primes[i] - log_primes[j]);
+                if (diff < min_log) {
+                    min_log = diff;
+                    prime_pair[i] = j;
+                }
+            }
+        }
+        for (int i = 0; i < num; i ++){
+            std::cout << "ASDFA " << prime_pair[i] << std::endl;
+        }
+
+        std::cout << std::hex;
+        int num_pair = 0;
+        uint64 max_sf = 0, min_sf = (uint64) 0xFFFFFFFFFFFFFFFF;
+        std::cout << max_sf << std::endl;
+        std::cout << min_sf << std::endl;
+        for (int i = 0; i < num; i ++) {
+            if (prime_pair[i] < i)
+                break;
+            if (prime_pair[prime_pair[i]] == i) {
+                uint64 sf = (uint64) primes[i] * primes[prime_pair[i]];
+                if (sf < min_sf)
+                    min_sf = sf;
+                if (sf > max_sf)
+                    max_sf = sf;
+                std::cout << sf << std::endl;
+                num_pair ++;
             }
         }
 
-        std::cout << num << std::endl;
-
-        for (int i = 0; i < 30; i ++) {
-            std::cout << primes[i] * primes[-i] << std::endl;
-        }
-
+        std::cout << num_pair << std::endl;
+        std::cout << max_sf << std::endl;
+        std::cout << min_sf << std::endl;
+        std::cout << (long double) max_sf / min_sf << std::endl;
 
     }
 
